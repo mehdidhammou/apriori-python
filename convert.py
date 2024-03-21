55efdaf21c99006263ba8db8c0c7b43070e83c82
mehdidhammou <mehdidhammou@protonmail.com>
2024-03-21 11:38:15 +0100

    changes

--- convert.py
+++ convert.py
@@ -9,7 +9,7 @@ binarized_df = (
 )
 
 # Convert True and False to t and ''
-binarized_df = binarized_df.replace({True: "t", False: ""})
+binarized_df = binarized_df.replace({True: 1, False: 0})
 
 # Save to CSV file
 binarized_df.to_csv("transactions_binarized.csv", index=False, header=True)

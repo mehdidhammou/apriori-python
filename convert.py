c57b4fe226328070ed6acf2c9adbb7851976a6c0
mehdidhammou <mehdidhammou@protonmail.com>
2024-03-21 04:35:34 +0100

    init

---
+++ convert.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Read the original transactional CSV file
+df = pd.read_csv("Market_Basket_Optimisation.csv", header=None)
+
+# Binarize the transactions
+binarized_df = (
+    pd.get_dummies(df.stack(), prefix="", prefix_sep="").groupby(level=0).max()
+)
+
+# Convert True and False to t and ''
+binarized_df = binarized_df.replace({True: "t", False: ""})
+
+# Save to CSV file
+binarized_df.to_csv("transactions_binarized.csv", index=False, header=True)
+
+print("Binarized CSV file created successfully!")

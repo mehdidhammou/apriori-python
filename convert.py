1a7e3559797c1d498ddc7835702605750aa12798
mehdidhammou <mehdidhammou@protonmail.com>
2024-03-29 00:32:38 +0100

    DONE

--- convert.py
+++ convert.py
@@ -1,17 +1,63 @@
+import warnings
+
+warnings.filterwarnings("ignore")
 import pandas as pd
+import sys
+import os
 
-# Read the original transactional CSV file
-df = pd.read_csv("Market_Basket_Optimisation.csv", header=None)
 
-# Binarize the transactions
-binarized_df = (
-    pd.get_dummies(df.stack(), prefix="", prefix_sep="").groupby(level=0).max()
-)
+def read_file(filename):
+    """
+    Reads a file based on its extension and returns a pandas DataFrame.
+    Supported formats: CSV, Excel (xlsx), and JSON.
+    """
+    # Mapping of file extensions to pandas read functions
+    read_functions = {
+        ".csv": pd.read_csv,
+        ".xls": pd.read_excel,
+        ".xlsx": pd.read_excel,
+        ".json": pd.read_json,
+    }
 
-# Convert True and False to t and ''
-binarized_df = binarized_df.replace({True: 1, False: 0})
+    _, file_extension = os.path.splitext(filename)
+    file_extension = file_extension.lower()
 
-# Save to CSV file
-binarized_df.to_csv("transactions_binarized.csv", index=False, header=True)
+    if file_extension in read_functions:
+        return read_functions[file_extension](filename, header=None)
+    else:
+        raise ValueError(f"Unsupported file format: {file_extension}")
 
-print("Binarized CSV file created successfully!")
+
+def main(filename):
+
+    try:
+        df = read_file(filename)
+
+        # Binarize the transactions
+        binarized_df = (
+            pd.get_dummies(df.stack(), prefix="", prefix_sep="").groupby(level=0).max()
+        )
+
+        # Convert True and False to 1 and 0
+        binarized_df = binarized_df.replace({True: 1, False: 0})
+
+        # Create a new filename by appending "_binarized" before the file extension
+        base_name, extension = os.path.splitext(filename)
+        new_name = f"{base_name}_binarized{extension}"
+
+        # Save to CSV file
+        binarized_df.to_csv(new_name, index=False, header=True)
+
+        print(f"Binarized file '{new_name}' created successfully!")
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    # Check if the filename was passed as an argument
+    if len(sys.argv) < 2:
+        print("Usage: python script_name.py <filename>")
+        sys.exit(1)
+
+    main(sys.argv[1])
